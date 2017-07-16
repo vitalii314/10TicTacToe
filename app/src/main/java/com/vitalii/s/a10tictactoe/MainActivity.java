1a8c3fbba422296f9707ef5db0e81c853facbfb2
Vitalik <s.vitalii314@gmail.com>
2017-07-16 13:30:33 +0300

    Navigation Drawer added ( not finished )

--- app/src/main/java/com/vitalii/s/a10tictactoe/MainActivity.java
+++ app/src/main/java/com/vitalii/s/a10tictactoe/MainActivity.java
@@ -1,9 +1,15 @@
 package com.vitalii.s.a10tictactoe;
 
 import android.app.Activity;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,7 +17,8 @@ import android.widget.Toast;
 
 import playground.SimplePlayGround;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     private GameViewStatic gameView;
     public TextView scoreText;
@@ -24,8 +31,56 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         gameView = (GameViewStatic)findViewById(R.id.viewStatic);
         scoreText = (TextView) findViewById(R.id.scoreText);
-        scoreText.setText("Score:" + GameViewStatic.playerWin+":"+GameViewStatic.compWin);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+            Toast.makeText(this, "PRESSED", Toast.LENGTH_LONG).show();
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
 
 
 //    @Override
